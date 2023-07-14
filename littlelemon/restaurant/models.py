dec27c7db7df8a86ec6db1e4ea7b2da3f499eb05
leyicao <leyicao@gmail.com>
2023-07-14 16:03:50 -0700

    add testcases

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -11,5 +11,5 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.SmallIntegerField()
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.title} : {str(self.price)}"
--- littlelemon/tests/test_models.py
+++ littlelemon/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from restaurant.models import Menu
 
-class MenuTest(TestCase):
+class MenuTestCase(TestCase):
     def test_get_item(self):
         item = Menu.objects.create(title='Meatball', price=7.25, inventory=15)
         self.assertEqual(item, "Meatball : 7.25")
